1cca1155284dc1b52981fbeec08e958ca7aaf4f8
viliusramulionis <35171606+viliusramulionis@users.noreply.github.com>
2023-08-16 11:27:55 +0300

    react components

---
+++ 2023-08-16/src/components/header/Header.js
@@ -0,0 +1,21 @@
+import './Header.css';
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <h1>Sveiki Visi</h1>
+            </header>
+            <nav>
+                <ul>
+                    <li><a href="#">Titulinis</a></li>
+                    <li><a href="#">Apie Mus</a></li>
+                    <li><a href="#">Informacija</a></li>
+                    <li><a href="#">Kontaktai</a></li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+export default Header;
